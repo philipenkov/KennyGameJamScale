685123e94d3a5488ba9ff3b794cbb4f0d48629d7
philipenkov <70628017+philipenkov@users.noreply.github.com>
2026-07-19 16:06:54 +0300

    ref: sink anim to program refactor

--- Assets/Scripts/ShipSinkAnimator.cs
+++ Assets/Scripts/ShipSinkAnimator.cs
@@ -1,18 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class ShipSinkAnimator : MonoBehaviour
 {
-    private Animator _animator;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-        _animator.enabled = false;
-    }
-
     public void PlayAnimation()
     {
-        _animator.enabled = true;
+        StartCoroutine(SinkRoutine());
+    }
+
+    private IEnumerator SinkRoutine()
+    {
+        float rotateDuration = 0.5f;
+        float sinkDuration = 1.0f;
+
+        Vector3 startPos = transform.localPosition;
+        Quaternion startRot = transform.localRotation;
+
+        Quaternion targetRot = startRot * Quaternion.Euler(30f, 0f, 0f);
+        Vector3 targetPos = startPos + new Vector3(0f, -15f, 0f);
+
+        float elapsed = 0f;
+        while (elapsed < rotateDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / rotateDuration;
+            transform.localRotation = Quaternion.Slerp(startRot, targetRot, t);
+            yield return null;
+        }
+        transform.localRotation = targetRot;
+
+        elapsed = 0f;
+        while (elapsed < sinkDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / sinkDuration;
+            transform.localPosition = Vector3.Lerp(startPos, targetPos, t);
+            yield return null;
+        }
+        transform.localPosition = targetPos;
     }
 }
